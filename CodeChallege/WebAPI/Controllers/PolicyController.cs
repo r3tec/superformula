f5b9b90203abf95f5c5442d370de5a47191f26b0
Alex Rozenbaum <alexander.rozenbaum@velocitor.com>
2022-02-09 11:17:36 -0500

    Initialize solution Add Policy controller Add ping and custom error

---
+++ CodeChallege/WebAPI/Controllers/PolicyController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Extensions;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PolicyController : ControllerBase
+    {
+        private readonly ILogger<PolicyController> _logger;
+
+        public PolicyController(ILogger<PolicyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Ping")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(CustomErrorResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Ping()
+        {
+            try
+            {
+                //using (SqlConnection conn = await DatabaseAccess.CreateSqlConnection(_databaseSettings.Value.ConnectionString).ConfigureAwait(false))
+                //{
+                //    // just make sure we can retrieve data from the database
+                //    int count = SqlUtils.ExecuteScalar<int>(conn, "SELECT COUNT(*) FROM [dbo].[DocumentTypes]", CommandType.Text);
+                //}
+                throw new Exception("No db yet"); 
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Application failed to communicate with database.");
+                ModelState.AddModelError("Error", ex.Message);
+                return new BadRequestObjectResult(new CustomErrorResponse(ControllerContext));
+            }
+        }
+
+    }
+}
---
+++ CodeChallege/WebAPI/CustomErrorResponse.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Extensions
+{
+    public class CustomErrorResponse
+    {
+        [JsonProperty("errors")]
+        public Dictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
+        [JsonProperty("status")]
+        public int Status { get; set; } = StatusCodes.Status400BadRequest;
+
+        public CustomErrorResponse(ActionContext context)
+        {
+            ConstructErrorMessages(context);
+        }
+
+        private void ConstructErrorMessages(ActionContext context)
+        {
+            Errors = context.ModelState.ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value.Errors?.Select(e => string.IsNullOrEmpty(e.ErrorMessage) ? "Invalid input." : e.ErrorMessage)?.ToArray() ?? Array.Empty<string>()
+                );
+        }
+
+    }
+}
